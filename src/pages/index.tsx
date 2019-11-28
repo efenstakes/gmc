21361c9b29f49d49ab14f446868befa467b4257e
Greg <mianzegregory@outlook.com>
2019-11-28 22:39:14 +0300

    started home page layout... Finish up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './styles.css'
 
 const Landing: React.FC = () => {
     return (
-        <div>
-            Landing Works
-            
+        <div className="page">
+            {/* top image + welcome text */}
+            <div className="top-img-text-container">
+                <div>Image here</div>
+                <div>Text here</div>
+            </div>
+
+            {/* container with highlighted text */}
+            <div className="blue-container">
+                <p>Lorem ipsum simut dolor amet</p>
+            </div>
+
         </div>
     )
 }
