0ec928b10a1e59d124e991d2bfbb2f6e1266ca74
Greg <mianzegregory@outlook.com>
2019-11-30 01:16:01 +0300

    home page full layout... TODO: add actual content now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,69 @@
 import React from 'react';
 import './styles.css'
+import Footer from '../components/footer/footer'
 
 const Landing: React.FC = () => {
     return (
         <div className="page">
             {/* top image + welcome text */}
             <div className="top-img-text-container">
-                <div>Image here</div>
                 <div>Text here</div>
+                <div>Image here</div>
             </div>
+
+            <br /><br />
 
             {/* container with highlighted text */}
             <div className="blue-container">
-                <p>Lorem ipsum simut dolor amet</p>
+                <div className="text-container"><p>Lorem ipsum simut dolor amet</p><p>Lorem ipsum simut dolor amet</p></div>
+                <div className="text-container"><p>Lorem ipsum simut dolor amet</p><p>Lorem ipsum simut dolor amet</p></div>
+                <div className="text-container"><p>Lorem ipsum simut dolor amet</p><p>Lorem ipsum simut dolor amet</p></div>
             </div>
+
+            <br /><br />
+
+            {/* container with reasons to join gmc */}
+            <div className="reasons-to-join">
+                <div className="isometric-and-text-container">
+                    <div>Image here</div>
+                    <div>Text here</div>
+                </div>
+                <br />
+
+                <div className="isometric-and-text-container">
+                    <div>Image here</div>
+                    <div>Text here</div>
+                </div>
+                <br />
+
+                <div className="isometric-and-text-container">
+                    <div>Image here</div>
+                    <div>Text here</div>
+                </div>
+                <br />
+
+                <div className="isometric-and-text-container">
+                    <div>Image here</div>
+                    <div>Text here</div>
+                </div>
+            </div>
+            <br /><br />
+
+            {/* partners section */}
+            <div className="partners">
+                <p>Partners</p>
+                <br />
+                <div className="partners-row">
+                    <div>Bidco</div>
+                    <div>KLM</div>
+                    <div>Clarity4D</div>
+                    <div>Incentro</div>
+                    <div>View All Btn</div>
+                </div>
+            </div>
+
+            {/* page footer */}
+            <Footer />
 
         </div>
     )
