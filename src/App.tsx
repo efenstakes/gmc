98bc70421c9d7825cb7c012d92bfb9467c2baf36
Greg <mianzegregory@outlook.com>
2019-12-20 10:07:56 +0300

    routing in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import Toolbar from './components/toolbar/toolbar'
-import Footer from './components/footer/footer'
-import Landing from './pages/index'
-import Challenges from './pages/challenges/index'
-import Contact from './pages/contact'
-import About from './pages/about'
-import RegisterIndex from './pages/register/index'
+import React from "react";
+import Toolbar from "./components/toolbar/toolbar";
+import Footer from "./components/footer/footer";
+import Landing from "./pages/index";
+import Challenges from "./pages/challenges/index";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import RegisterIndex from "./pages/register/index";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Toolbar />
-
-      {/* <Landing />
-      <Challenges />
-      <Contact /> 
-      <About /> */}
-      <RegisterIndex />
-
-      {/* <Footer /> */}
+      <BrowserRouter>
+        <Toolbar />
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/challenges" exact component={Challenges} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/about" exact component={About} />
+          <Route path="/register" exact component={RegisterIndex} />
+          <Route path="/" render={() => <div>404</div>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 const Toolbar: React.FC = () => {
-    return (
-        <div className="toolbar">
-            <img className="logo" src="https://ik.imagekit.io/sgmianze96/gmc/logo_SYBSgDHHV.svg" alt="Logo" />
-            <nav>
-                <ul className="nav_links">
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">CHALLENGES</a></li>
-                    <li><a href="#">ABOUT US</a></li>
-                    <li><a href="#">TALK TO US</a></li>
-                </ul>
-            </nav>
-            <a className="btn" href="#"><button>SIGN UP</button></a>
-        </div>
-    )
+  return (
+    <div className="toolbar">
+      <Link to="/">
+        <img
+          className="logo"
+          src="https://ik.imagekit.io/sgmianze96/gmc/logo_SYBSgDHHV.svg"
+          alt="Logo"
+        />
+      </Link>
 
-}
+      <nav>
+        <ul className="nav_links">
+          <Link to="/">
+            <li>
+              <a>HOME</a>
+            </li>
+          </Link>
+          <Link to="/challenges">
+            <li>
+              <a>CHALLENGES</a>
+            </li>
+          </Link>
+          <Link to="/about">
+            <li>
+              <a>ABOUT US</a>
+            </li>
+          </Link>
+          <Link to="/contact">
+            <li>
+              <a>TALK TO US</a>
+            </li>
+          </Link>
+        </ul>
+      </nav>
+      <Link to="/register">
+        <a className="btn">
+          <button>SIGN UP</button>
+        </a>
+      </Link>
+    </div>
+  );
+};
 
-export default Toolbar
+export default Toolbar;
