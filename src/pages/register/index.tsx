53c5fcf7e2ed16a22e3d36746ac2bb4c41cf42d9
Greg <mianzegregory@outlook.com>
2019-12-27 19:59:06 +0300

    register page fix

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Page } from "../../components/styled_components";
-import Row from "../../components/layout/Row"; 
+import Row from "../../components/layout/Row";
+import Button from "../../components/common/Button";
+import TitleText from "../../components/common/TitleText";
 
 export default class RegisterIndex extends React.Component {
+  private buttonStyle = {
+    width: "35vw",
+    height: "80px"
+  };
   render() {
     return (
       <Page>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <h2>Who are we looking for?</h2>
+        <TitleText
+          style={{ textAlign: "center" }}
+          text="Who are we looking for?"
+        />
         <br />
         <h3>
           Are you a go-getter, a dreamer or a believer? or maybe even a square
@@ -22,13 +25,8 @@ export default class RegisterIndex extends React.Component {
         <br />
         <br />
         <Row>
-          <button style={{ width: "40%", height: "80px" }}>
-            Company application
-          </button>
-
-          <button style={{ width: "40%", height: "80px" }}>
-            Wildcard application
-          </button>
+          <Button text="Company application" style={this.buttonStyle} />
+          <Button text="Wildcard application" style={this.buttonStyle} />
         </Row>
       </Page>
     );
