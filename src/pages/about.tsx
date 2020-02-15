4a8c239c1db8733cbf4d363b2cbdb3f185d202a0
Gregory <mianzegregory@outlook.com>
2020-02-15 20:27:00 +0300

    popup for people avatar pc

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Popup from "reactjs-popup";
 
 import Text from "../components/common/Text";
 import Row from "../components/layout/Row";
@@ -13,8 +14,19 @@ const About: React.FC = () => {
   const colWidth: string = "40%";
 
   //default image height and width
-  const imgHeight: string = "300px";
-  const imgWidth: string = "85%";
+  const imgHeight: string = "280px";
+  const imgWidth: string = "80%";
+
+  //content of modal
+  const modalContent: any = (
+    <ModalWrapper>
+      <Text isBody>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris.
+      </Text>
+    </ModalWrapper>
+  );
 
   return window.screen.width < 740 ? (
     <MobileAbout />
@@ -60,54 +72,62 @@ const About: React.FC = () => {
       </Row>
       <br />
       <br />
-      <Row isDefault={true}>
+      <Row isDefault>
         <Column maxWidth={colWidth} minWidth={colWidth}>
           <Text isTitle>Meet our team</Text>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg"
-                height="60px"
-                width="60px"
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg"
-                height="60px"
-                width="60px"
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg"
-                height="60px"
-                width="60px"
-              />
-            </div>
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg" />
+              }
+            >
+              {modalContent}
+            </Popup>
           </Row>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg"
-                height="60px"
-                width="60px"
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg"
-                height="60px"
-                width="60px"
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg"
-                height="60px"
-                width="60px"
-              />
-            </div>
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/p3l/avatar_yHHj_DT5a.svg" />
+              }
+            >
+              {modalContent}
+            </Popup>
           </Row>
         </Column>
         <Column maxWidth={colWidth} minWidth={colWidth}>
@@ -121,7 +141,7 @@ const About: React.FC = () => {
       </Row>
       <br />
       <br />
-      <Row isDefault={true}>
+      <Row isDefault>
         <Column maxWidth={colWidth} minWidth={colWidth}>
           <Image
             id="inspiration"
@@ -132,52 +152,55 @@ const About: React.FC = () => {
         </Column>
         <Column maxWidth="55%" minWidth="55%">
           <Text isTitle>Board of inspiration</Text>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/ruthie_rono_FnRnN5DxX.jpeg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/eddie_sarroukh_jTBfCXFzT.jpeg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/wahu_kagwi_FjZBA0YlR.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/ruthie_rono_FnRnN5DxX.jpeg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/eddie_sarroukh_jTBfCXFzT.jpeg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/wahu_kagwi_FjZBA0YlR.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
           </Row>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/vimal_shah_NB82_BJcx.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/jane_ngige_TWpF1BE5M.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div></div>
-
-            <div></div>
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/vimal_shah_NB82_BJcx.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/inspiration_board/jane_ngige_TWpF1BE5M.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Image
+              src="http://www.ringling.edu/sites/default/files/Blank_Canvas_on_Transparent_Background_0.png"
+              height="70px"
+              width="70px"
+              style={{ borderRadius: "50%" }}
+            />
           </Row>
         </Column>
       </Row>
@@ -186,93 +209,96 @@ const About: React.FC = () => {
       <Row isDefault={true}>
         <Column maxWidth={colWidth} minWidth={colWidth}>
           <Text isTitle>Coaches & Experts</Text>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/decimar_ogutu_JPJW0MxSL.jpeg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/david_bernard-stevens_odas_jGE8.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/aart_bos_Z9QDWFTi5.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/decimar_ogutu_JPJW0MxSL.jpeg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/david_bernard-stevens_odas_jGE8.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/aart_bos_Z9QDWFTi5.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
           </Row>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/mart-veeken_6fk15WB7d.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/susan-wamae_YsLgO55kh.png"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/dennis_de_weed_nFkBwobYl.png"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
+
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/mart-veeken_6fk15WB7d.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/susan-wamae_YsLgO55kh.png" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/dennis_de_weed_nFkBwobYl.png" />
+              }
+            >
+              {modalContent}
+            </Popup>
           </Row>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/irene_nchama_nDWrkoF0d.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/derek_bbanga_WZ26WVBpR.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/sheetal_shah_O2Cu1LH7w.jpeg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
+
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/irene_nchama_nDWrkoF0d.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/derek_bbanga_WZ26WVBpR.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/sheetal_shah_O2Cu1LH7w.jpeg" />
+              }
+            >
+              {modalContent}
+            </Popup>
           </Row>
-          <Row isDefault={true}>
-            <div className="person_avatar">
-              <Image
-                src="https://ik.imagekit.io/sgmianze96/gmc/coaches/madelon_barens_tqHXxjgI1.jpg"
-                height="60px"
-                width="60px"
-                style={{ borderRadius: "50%" }}
-              />
-            </div>
+
+          <Row isDefault>
+            <Popup
+              modal
+              trigger={
+                <Avatar src="https://ik.imagekit.io/sgmianze96/gmc/coaches/madelon_barens_tqHXxjgI1.jpg" />
+              }
+            >
+              {modalContent}
+            </Popup>
           </Row>
         </Column>
         <Column maxWidth={colWidth} minWidth={colWidth}>
@@ -286,7 +312,7 @@ const About: React.FC = () => {
       </Row>
       <br />
       <br />
-      <Row isDefault={true}>
+      <Row isDefault>
         <Column maxWidth={colWidth} minWidth={colWidth}>
           <Image
             id="mission"
@@ -326,14 +352,10 @@ const Wrapper = styled.div`
   padding-top: 100px;
   max-width: 80%;
   margin: auto auto;
-
-  .person_avatar {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
+  .popup-content {
+    border-radius: 20px;
+    min-width: 70vw;
   }
-
   @media (max-width: 1025px) {
     #story,
     #why_us,
@@ -362,4 +384,27 @@ const Wrapper = styled.div`
       height: 320px;
     }
   }
+`;
+
+const Avatar = styled.img`
+  height: 70px;
+  width: 70px;
+  border-radius: 50% !important;
+  :hover {
+    box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2),
+      0 3px 15px 0 rgba(0, 0, 0, 0.19);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+  }
+`;
+
+const ModalWrapper = styled.div`
+  background-color: white;
+  * {
+    background-color: transparent;
+  }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 40px 40px;
 `;
