1b41f9db1ed4f6f17c3d38927ed5bf0d6341a388
Efen Stakes <efenstakes101@gmail.com>
2020-03-06 14:50:44 +0300

    Updates

---
+++ precache-manifest.54c92a1928a7067214f07daf28115515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eebf228bf9dc151389ae119588170f0",
+    "url": "/gmc/index.html"
+  },
+  {
+    "revision": "046eac48f2cce4253b8a",
+    "url": "/gmc/static/css/main.d2b7eb38.chunk.css"
+  },
+  {
+    "revision": "932636b49c650070c06b",
+    "url": "/gmc/static/js/2.40ffd599.chunk.js"
+  },
+  {
+    "revision": "16f6266331faea4651e604f884e88e18",
+    "url": "/gmc/static/js/2.40ffd599.chunk.js.LICENSE"
+  },
+  {
+    "revision": "046eac48f2cce4253b8a",
+    "url": "/gmc/static/js/main.3748af1b.chunk.js"
+  },
+  {
+    "revision": "6e09d1c20c0398b270c5",
+    "url": "/gmc/static/js/runtime-main.a7827277.js"
+  }
+]);
